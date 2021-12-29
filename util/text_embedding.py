3dba2bd5c2c2b43639145a51f4de2e3fcd49f713
achikina@isgneuro.com <KI38o*Kw>
2021-12-29 16:52:17 +0300

    rewrite find_sim_texts function

--- util/text_embedding.py
+++ util/text_embedding.py
@@ -17,12 +17,12 @@ def embed_bert_cls(text, model_path, tokenizer_path):
     return embeddings[0].cpu().numpy()
 
 
-def find_sim_texts(texts, input_vec, ref_number, similarity_threshold):
+def find_sim_texts(texts, input_vec, ref_number, similarity_threshold, full_output=False):
     archive_vecs = np.array([x.tolist() for x in texts["bert_vec"].values])
     similarity_matrix = cosine_similarity(input_vec.reshape(1, -1), archive_vecs)
     sim_texts = list(filter(lambda x: x[1] > similarity_threshold,
                             list(zip(texts["art_ind"], similarity_matrix[0]))))
-    if len(sim_texts) > ref_number:
+    if len(sim_texts) > ref_number and not full_output:
         sim_texts = sorted(sim_texts, key=lambda x: x[1], reverse=True)[:ref_number]
-    sim_ind = [x[0] for x in sim_texts]
-    return texts.set_index("art_ind").loc[sim_ind]
+    # sim_ind = [x[0] for x in sim_texts]
+    return sim_texts
