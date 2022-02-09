8abb2b641ae751e41ae3ac84d051208a080c6644
achikina@isgneuro.com <KI38o*Kw>
2022-02-09 15:54:44 +0300

    filtration and get_text_features are done with OTP now

--- context_gen.py
+++ context_gen.py
@@ -1,13 +1,11 @@
-import time
+# import time
 
-# import pandas as pd
+import numpy as np
 import streamlit as st
 from envyaml import EnvYAML
-from transformers import AutoTokenizer, AutoModel
 
-from util import text_embedding, abstractive_summarization, extractive_summarization, grammar_check, kwne_similarity, \
-    data_preprocessing, textrank, ner_finder
-from util.otp_connector import get_dedup_articles, get_keywords, get_embedding
+from util import extractive_summarization, grammar_check, text_embedding, kwne_similarity
+from util.otp_connector import get_text_features_eep, get_unique_source_topics, get_filtered_articles
 
 config = EnvYAML("config_local.yaml")
 bert_embedding_path = config["models"]["embedding"]
@@ -18,26 +16,14 @@ ref_num_default = int(config["params"]["reference_number"])
 sent_num_default = int(config["params"]["sentence_number"])
 data_path = config["connection"]["data_path"]
 
-# archive_texts = pd.read_parquet(archive_path)
-archive_texts = get_dedup_articles(data_path)
 grammar_tool = grammar_check.download_tool()
 
 
 def get_text_features(input_text):
-    input_noun_phrases = data_preprocessing.collect_np(input_text, ru_sw_file)
-    input_kw = textrank.text_rank(input_noun_phrases, 15)
-    kw_df, kw_filename = get_keywords(input_text.replace("\n", " ").replace("\r", " ").replace('"', ''))
-    otp_kw = kw_df["keywords"].values[0]
-    input_ne = ner_finder.finder(input_text)
-    input_kw_ne = kwne_similarity.unite_kw_ne(input_kw, input_ne)
-
-    tokenizer = AutoTokenizer.from_pretrained(bert_embedding_path)
-    model = AutoModel.from_pretrained(bert_embedding_path)
-    # TODO: add file not found error
-    input_vec = text_embedding.embed_labse(input_text, tokenizer, model)
-    vec_df, vec_filename = get_embedding(kw_filename)
-    otp_vec = vec_df["embedding"].values[0]
-    return input_vec, input_kw_ne
+    input_df = get_text_features_eep(input_text.replace("\n", " ").replace("\r", " ").replace('"', ''))
+    input_kw_ne = set(input_df["kw_ne"].values[0].split("; "))
+    input_vec = np.array(input_df["embedding"].values[0].split("; "))
+    return input_kw_ne, input_vec
 
 
 def check_grammar_on_click(input_text: str, grammar_container: st.container):
@@ -51,30 +37,16 @@ def check_grammar_on_click(input_text: str, grammar_container: st.container):
         del grammar_container
 
 
-def filter_params_form():
+def filter_params_form(path):
     st.subheader('Параметры фильтрации документов')
-
-    sources_list = sorted(archive_texts["source"].unique())
+    sources_list = get_unique_source_topics(path, "source")["source"].values
     sources = st.multiselect('Выберите источник', sources_list)
-    if len(sources) > 0:
-        filtered_df = archive_texts[archive_texts["source"].apply(lambda s: s in sources)]
-    else:
-        filtered_df = archive_texts.copy()
-
     dates = st.date_input("Задайте период поиска", value=[])
-    if len(dates) > 0:
-        start, end = dates
-        start = time.mktime(start.timetuple())
-        end = time.mktime(end.timetuple())
-        filtered_df = archive_texts[(archive_texts["_time"] >= start) & (archive_texts["_time"] <= end)]
-    return filtered_df
+    return sources, dates
 
 
 def context_params_form():
     st.subheader('Настройки генерации бекграунда')
-
-    summ_type = st.selectbox('Выберите способ',
-                             ["Экстрактивная суммаризация", "Абстрактивная суммаризация"])
 
     ref_num = st.slider("Выберите максимальное число документов для генерации бекграунда",
                         min_value=1,
@@ -87,44 +59,35 @@ def context_params_form():
                          max_value=5,
                          value=sent_num_default,
                          step=1)
-    return summ_type, ref_num, sent_num  # sim_threshold,
+    return ref_num, sent_num  # summ_type,
 
 
-def generate_context(filtered_df, input_vec, input_kw_ne, ref_num, summ_type, sent_num):  # sim_threshold,
-    if len(filtered_df) == 0:
+def generate_context(path, dates, sources, input_vec, input_kw_ne, ref_num, sent_num):
+    filtered_df = get_filtered_articles(path, dates, sources, input_kw_ne)
+    cos_sim_ind_score = text_embedding.find_sim_texts(filtered_df.dropna(),
+                                                      input_vec,
+                                                      ref_num,
+                                                      full_output=True)
+    filtered_df = filtered_df.set_index("art_ind")
+    sim_ind_score = kwne_similarity.get_kwne_sim(input_kw_ne, filtered_df, cos_sim_ind_score)
+    sim_ind = [x[0] for x in sim_ind_score][:ref_num]
+    sim_texts_df = filtered_df.loc[sim_ind]
+
+    if len(sim_texts_df) == 0:
         st.write("Я не нашёл подходящие под параметры фильтрации тексты. Попробуйте поменять настройки.")
-    elif len(filtered_df) > 0:
-        cos_sim_ind_score = text_embedding.find_sim_texts(filtered_df.dropna(),
-                                                          input_vec,
-                                                          ref_num,
-                                                          full_output=True)  # similarity_threshold,
-        filtered_df = filtered_df.set_index("art_ind")
-        sim_ind_score = kwne_similarity.get_kwne_sim(input_kw_ne, filtered_df, cos_sim_ind_score)
-        sim_ind = [x[0] for x in sim_ind_score][:ref_num]
-        sim_texts_df = filtered_df.loc[sim_ind]
 
-        if len(sim_texts_df) == 0:
-            st.write("Я не нашёл подходящие под параметры фильтрации тексты. Попробуйте поменять настройки.")
+    sim_texts = sim_texts_df[["clean_text", "text"]].values.tolist()
+    art_inds = sim_texts_df.index
+    urls = sim_texts_df["url"].values
 
-        sim_texts = sim_texts_df[["clean_text", "text"]].values.tolist()
-        art_inds = sim_texts_df.index
-        urls = sim_texts_df["url"].values
-
-        if summ_type == "Абстрактивная суммаризация":
-            context = abstractive_summarization.sum_text(sim_texts, rut5_ru_sum_path)
-            for i, doc in enumerate(context):
-                st.write(str(i + 1))
-                st.write(doc)
-                st.write("\n\n")
-        else:
-            context = extractive_summarization.lexrank_sum(sim_texts, sent_num=sent_num)
-            for i, doc in enumerate(context):
-                st.markdown("[%s](%s)" % (art_inds[i], urls[i]))
-                output = ""
-                for sentence in doc:
-                    output += sentence
-                    output += " "
-                st.write(output)
+    context = extractive_summarization.lexrank_sum(sim_texts, sent_num=sent_num)
+    for i, doc in enumerate(context):
+        st.markdown("[%s](%s)" % (art_inds[i], urls[i]))
+        output = ""
+        for sentence in doc:
+            output += sentence
+            output += " "
+        st.write(output)
 
 
 def load_page():
@@ -133,7 +96,7 @@ def load_page():
     input_text = st.text_area('Начните набирать текст в поле. '
                               'Когда текст готов, задайте настройки и нажмите  "%s"' % button_name,
                               height=700)
-    input_vec, input_kw_ne = get_text_features(input_text)
+    input_kw_ne, input_vec = get_text_features(input_text)
 
     grammar_button = st.button("Проверить правописание")
     grammar_container = st.container()
@@ -145,12 +108,12 @@ def load_page():
         with st.form("params_form"):
             col1, col2 = st.columns(2)
             with col1:
-                filtered_df = filter_params_form()
+                sources, dates = filter_params_form(data_path)
             with col2:
-                summ_type, ref_num, sent_num = context_params_form()  # sim_threshold,
+                ref_num, sent_num = context_params_form()
             st.form_submit_button("Применить настройки")
 
     gen_button = st.button(button_name)
 
     if gen_button:
-        generate_context(filtered_df, input_vec, input_kw_ne, ref_num, summ_type, sent_num)  # sim_threshold,
+        generate_context(data_path, dates, sources, input_vec, input_kw_ne, ref_num, sent_num)
