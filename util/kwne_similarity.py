814d22cc28e7826c1f083ee5a55505e1a14f88a9
achikina@isgneuro.com <KI38o*Kw>
2022-02-09 15:56:34 +0300

    change var type from list to str for kw_ne

--- util/kwne_similarity.py
+++ util/kwne_similarity.py
@@ -13,7 +13,7 @@ def unite_kw_ne(input_kw: list, input_ne: list):
 def get_kwne_sim(input_kw_ne: set, archive_df: pd.DataFrame, sim_texts: list):
     sim_ind = [x[0] for x in sim_texts]
     sim_score = [x[1] for x in sim_texts]
-    sim_kw_ne = [set(kw) for kw in archive_df.loc[sim_ind, "kw_ne"].values]
+    sim_kw_ne = [set(kw.split("; ")) for kw in archive_df.loc[sim_ind, "kw_ne"].values]
     common_kw_ne = np.array([len(kw & input_kw_ne) / len(kw | input_kw_ne) + 1.0 for kw in sim_kw_ne])
     similarities = sorted(list(zip(sim_ind, np.multiply(sim_score, common_kw_ne))), key=lambda x: x[1], reverse=True)
     return similarities
