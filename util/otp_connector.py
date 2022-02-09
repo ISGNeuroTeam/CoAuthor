24d518ce2059f9b5593bdd026fa038caff7eabfa
achikina@isgneuro.com <KI38o*Kw>
2022-02-09 15:55:13 +0300

    add OTL searches

--- util/otp_connector.py
+++ util/otp_connector.py
@@ -18,33 +18,42 @@ tws = 11
 twf = 22
 
 
-def get_dedup_articles(path):
-    query_text = "| readFile format=parquet path=%s | dedup art_ind | head 100" % path
+def get_filtered_articles(path, dates, sources, kw_ne):
+    # dates = (datetime.date(2022, 1, 1), datetime.date(2022, 1, 27))
+    query_text = '| readFile format=parquet path=%s' % path
+    # |eval kw_ne_temp = kw_ne | makemv delim=";" kw_ne_temp | mvexpand kw_ne_temp
+    # if len(kw_ne) > 0:
+    #     query_text += "| where " + " OR ".join([f'kw_ne="{kw}"' for kw in kw_ne])
+    if len(sources) > 0:
+        query_text += "| where " + " OR ".join([f'source="{source}"' for source in sources])
+    if len(dates) > 0:
+        start, end = dates
+        start = time.mktime(start.timetuple())
+        end = time.mktime(end.timetuple())
+        query_text += "| where _time>=%d AND _time<=%d" % (start, end)
+    query_text += "|sort -_time | head 2000"
     job = conn.jobs.create(query_text=query_text, cache_ttl=cache_ttl, tws=tws, twf=twf, blocking=True)
     return pd.DataFrame(job.dataset.load())
 
 
-def get_keywords(text):
-    filename = str(time.time()) + "###" + text[:10].replace(" ", "_")
+def get_unique_source_topics(path, feature):
+    query_text = "| readFile format=parquet path=%s | chart count by %s " % (path, feature)
+    job = conn.jobs.create(query_text=query_text, cache_ttl=cache_ttl, tws=tws, twf=twf, blocking=True)
+    result = pd.DataFrame(job.dataset.load())
+    return result
+
+
+def get_text_features_eep(text):
+    # filename = str(time.time()) + "###" + text[:10].replace(" ", "_")
     query_text = '| makeresults | eval text="%s" | runProcess script=kw_textrank_eep.kw_extraction ' \
-                 '| writeFile format=parquet path=%s ' % (text, filename)
-    job = conn.jobs.create(query_text=query_text, cache_ttl=cache_ttl, tws=tws, twf=twf, blocking=True)
-    return pd.DataFrame(job.dataset.load()), filename
-
-
-def get_embedding(filename):
-    new_filename = str(time.time()) + "###" + filename.split("###")[-1]
-    query_text = "| readFile format=parquet path=%s | runProcess script=doc_embedding.worker" \
-                 "| writeFile format=parquet path=%s " % (filename, new_filename)
-    job = conn.jobs.create(query_text=query_text, cache_ttl=cache_ttl, tws=tws, twf=twf, blocking=True)
-    return pd.DataFrame(job.dataset.load()), new_filename
-
-
-def sim_score(filename, archive_path):
-    query_text = '| readFile format = parquet path = %s | eval art_ind = "%s" | makemv embedding delim = "; "' \
-                 '| fields art_ind, text, embedding' \
-                 '| append [ | readFile format = parquet path = %s ' \
-                 '| rename bert_vec_labse as embedding | fields art_ind, text, embedding] | head 10' % (
-                     filename, filename, archive_path)
+                 '| runProcess script=doc_embedding.worker' % text  # | writeFile format=parquet path=%s
     job = conn.jobs.create(query_text=query_text, cache_ttl=cache_ttl, tws=tws, twf=twf, blocking=True)
     return pd.DataFrame(job.dataset.load())
+
+# def sim_score(filename, archive_path, ref_num):
+#     query_text = '| readFile format=parquet path=%s | append [ | readFile format=parquet path=%s ]' \
+#                 '| eval flag=if(art_ind==%s, 1, 0) | runProcess script=cosine_similarity.worker ' \
+#                  '| where flag=0 | fields art_ind, text, clean_text, url, sim_score | sort -sim_score ' \
+#                  '| head %d' % (filename, archive_path, filename, ref_num)
+#     job = conn.jobs.create(query_text=query_text, cache_ttl=cache_ttl, tws=tws, twf=twf, blocking=True)
+#     return pd.DataFrame(job.dataset.load())
