06165695f35d0ead52df5a0cff7295dd71be0cd3
achikina@isgneuro.com <KI38o*Kw>
2022-02-11 15:30:12 +0300

    change sidebar width + remove page select

--- menu.py
+++ menu.py
@@ -1,18 +1,39 @@
 import streamlit as st
 
 import context_gen
+import news_feed
 
 st.set_page_config(
     page_title="СоАвтор",
     layout="wide")
 
-st.sidebar.title("Лента новостей")
-st.sidebar.header("Здесь будет лента новостей")
+# change sidebar width
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        width: 500px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        width: 500px;
+        margin-left: -500px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
-page = st.selectbox("", ["Бекграунд", "Архив", "Аналитика"])
-if page == "Бекграунд":
-    context_gen.load_page()
-elif page == "Архив":
-    st.header("Загружается страница архива")
-elif page == "Аналитика":
-    st.header("Загружается страница аналитики")
+# output news feed to the sidebar
+with st.sidebar:
+    news_feed.load_page()
+
+# load context page
+context_gen.load_page()
+
+# page = st.selectbox("", ["Бекграунд", "Архив", "Аналитика"])
+# if page == "Бекграунд":
+#     context_gen.load_page()
+# elif page == "Архив":
+#     st.header("Загружается страница архива")
+# elif page == "Аналитика":
+#     st.header("Загружается страница аналитики")
