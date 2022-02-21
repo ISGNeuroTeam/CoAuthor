951d06a0942a955723d1b5dcd9d3dd3e88e7bd0a
achikina@isgneuro.com <KI38o*Kw>
2022-02-21 13:25:51 +0300

    add authentication

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from envyaml import EnvYAML
 
 import context_gen
 import news_feed
@@ -23,17 +24,62 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# output news feed to the sidebar
-with st.sidebar:
-    news_feed.load_page()
 
-# load context page
-context_gen.load_page()
+def is_authenticated(log, log_list):
+    return log in log_list
 
-# page = st.selectbox("", ["Бекграунд", "Архив", "Аналитика"])
-# if page == "Бекграунд":
-#     context_gen.load_page()
-# elif page == "Архив":
-#     st.header("Загружается страница архива")
-# elif page == "Аналитика":
-#     st.header("Загружается страница аналитики")
+
+def generate_blocks(element):
+    log_block = element.empty()
+    inf_block = element.empty()
+    vid_block = element.empty()
+    return log_block, inf_block, vid_block
+
+
+def clean_blocks(blocks):
+    for block in blocks:
+        block.empty()
+
+
+def login_form(block):
+    # TODO: add button for text input?
+    block_container = block.container()
+    block_container.image("media/Soavtor Logo.png", use_column_width=False, width=300)
+    block_container.subheader("Сосредоточься на истории, а не технических деталях")
+    block_container.write("\n\n\n")
+    return block_container.text_input("Для доступа к приложению введите адрес почты, который Вы вводили при регистрации, и нажмите Enter")
+
+
+def info_form(block, info=""):
+    if len(info) > 0:
+        block.info(info)
+
+
+def video_form(block):
+    block.video("media/streamlit-menu-2022-02-16-13-02-42.webm", format="video/webm", start_time=0)
+
+
+def main():
+    # output news feed to the sidebar
+    with st.sidebar:
+        news_feed.load_page()
+
+    # load context page
+    context_gen.load_page()
+
+
+_, center_col, _ = st.columns([1, 3, 1])
+login_block, info_block, video_block = generate_blocks(center_col)
+login = login_form(login_block)
+info_form(info_block)
+video_form(video_block)
+config = EnvYAML("config_local.yaml")
+login_list_path = config["authentication"]["login_list_path"]
+login_list = open(login_list_path, "r").read().split("\n")
+
+
+if is_authenticated(login, login_list):
+    clean_blocks([login_block, info_block, video_block])
+    main()
+elif login:
+    info_form(info_block, info="Такая почта не зарегистрирована")
