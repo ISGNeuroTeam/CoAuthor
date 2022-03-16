bb62fad8f59ed2fec918b6d60d19d1688a98e83c
achikina@isgneuro.com <KI38o*Kw>
2022-03-16 12:14:43 +0300

    add precheck for tass news

--- util/extractive_summarization.py
+++ util/extractive_summarization.py
@@ -8,7 +8,10 @@ def lexrank_sum(texts_set, sent_num=1):
     summarizer = LexRankSummarizer()
     summary = []
     for clean_text, text in texts_set:
-        temp_text = clean_text.replace("###SENT###", " .\n")
+        temp_text = clean_text.split("###SENT###")
+        if len(temp_text) > 2 and temp_text[1] == "тасс":
+            temp_text = temp_text[2:]
+        temp_text = ".\n".join([sentence for sentence in temp_text if len(sentence) > 3])
         doc_dict = dict(zip(clean_text.split("###SENT###"), sentenize(text)))
         my_parser = PlaintextParser.from_string(temp_text, Tokenizer('russian'))
         doc_sum = summarizer(my_parser.document, sentences_count=sent_num)
