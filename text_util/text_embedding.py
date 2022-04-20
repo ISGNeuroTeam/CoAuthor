d65ea6953b6d108473991ebc568d7a837c46c657
achikina@isgneuro.com <KI38o*Kw>
2022-04-20 14:24:12 +0300

    change text embedding to sentence embeddings

--- text_util/text_embedding.py
+++ text_util/text_embedding.py
@@ -3,21 +3,29 @@ import torch
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def embed_labse(text, tokenizer, model):
-    # , model_path, tokenizer_path
-    encoded_input = tokenizer(text, padding=True, truncation=True, max_length=64, return_tensors='pt')
+def sentences_to_embeddings(sentences, tokenizer, model):
+    encoded_input = tokenizer(sentences, padding=True, truncation=True, max_length=64, return_tensors='pt')
     with torch.no_grad():
         model_output = model(**encoded_input)
     embeddings = model_output.pooler_output
     embeddings = torch.nn.functional.normalize(embeddings)
-    return embeddings[0].cpu().numpy()
+    mean_embedding = np.mean(embeddings.numpy(), axis=0)
+    return mean_embedding
 
 
-def find_sim_texts(texts, input_vec, ref_number, full_output=False):  # similarity_threshold,
+# def embed_labse(text, tokenizer, model):
+#     # , model_path, tokenizer_path
+#     encoded_input = tokenizer(text, padding=True, truncation=True, max_length=64, return_tensors='pt')
+#     with torch.no_grad():
+#         model_output = model(**encoded_input)
+#     embeddings = model_output.pooler_output
+#     embeddings = torch.nn.functional.normalize(embeddings)
+#     return embeddings[0].cpu().numpy()
+
+
+def find_sim_texts(texts, input_vec, ref_number, full_output=False):
     archive_vecs = np.array([x.split("; ") for x in texts["embedding"].values])
     similarity_matrix = cosine_similarity(input_vec.reshape(1, -1), archive_vecs)
-    # sim_texts = list(filter(lambda x: x[1] > similarity_threshold,
-    #                         list(zip(texts["art_ind"], similarity_matrix[0]))))
     sim_texts = list(zip(texts["art_ind"], similarity_matrix[0]))
     if len(sim_texts) > ref_number and not full_output:
         sim_texts = sorted(sim_texts, key=lambda x: x[1], reverse=True)[:ref_number]
